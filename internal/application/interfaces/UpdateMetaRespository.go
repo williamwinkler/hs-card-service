42e38cb8c6801bb1b9782537a02c2a4fd1d15d39
williamwinkler <williambwinkler@gmail.com>
2023-01-16 13:58:05 +0100

    remove updateMetaRepo

--- internal/application/interfaces/UpdateMetaRespository.go
+++
@@ -1,8 +0,0 @@
-package interfaces
-
-import "github.com/williamwinkler/hs-card-service/internal/domain"
-
-type UpdateMetaRepository interface {
-	InsertOne(domain.CardMeta) error
-	FindNewest() (domain.CardMeta, error)
-}
