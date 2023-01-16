6b8b0eefd3fbf538cabebbd5f34da071a9cdc1cd
williamwinkler <williambwinkler@gmail.com>
2023-01-16 23:07:36 +0100

    🧱 Bad practice update of cards added...

--- internal/application/cardService.go
+++ internal/application/cardService.go
@@ -1,8 +1,7 @@
 package application
 
 import (
-	"log"
-	"sort"
+	"time"
 
 	"github.com/williamwinkler/hs-card-service/internal/application/interfaces"
 	"github.com/williamwinkler/hs-card-service/internal/domain"
@@ -52,78 +51,23 @@ func (c *CardService) GetRichCards(filter bson.M, page int, limit int) ([]domain
 }
 
 func (c *CardService) Update() error {
+	// TODO: make it smarter, so it only deletes/updates/adds affected cards
 	cards, err := c.hsClient.GetAllCards()
 	if err != nil {
 		return err
 	}
-	newCards := sortCardsByAlphabet(cards)
 
-	oldCards, err := c.cardRepo.FindAll()
+	err = c.cardRepo.DeleteAll()
 	if err != nil {
 		return err
 	}
 
-	newCardsMap := make(map[int]bool)
-	for _, card := range newCards {
-		newCardsMap[card.ID] = true
-	}
-
-	oldCardsMap := make(map[int]bool)
-	for _, card := range oldCards {
-		oldCardsMap[card.ID] = true
-	}
-
-	cardsMetaUpdate := domain.NewCardMeta()
-
-	// Remove old cards from the collection which are not in the new list
-	for i, card := range oldCards {
-		if !newCardsMap[card.ID] {
-			err := c.cardRepo.DeleteOne(card)
-			if err != nil {
-				log.Fatalf("Card %d failed to be deleted: %v", card.ID, err)
-				continue
-			}
-			log.Printf("Card %d was deleted", card.ID)
-			delete(oldCardsMap, card.ID)
-			oldCards = append(oldCards[:i], oldCards[i+1:]...)
-			cardsMetaUpdate.DeleteCard(card)
-		}
-	}
-
-	// Add new or update cards to the collection
-	for i, card := range newCards {
-		if !oldCardsMap[card.ID] {
-			err := c.cardRepo.InsertOne(card)
-			if err != nil {
-				log.Fatalf("Card %d failed to inserted: %v", card.ID, err)
-				continue
-			}
-			log.Printf("Card %d was added", card.ID)
-			cardsMetaUpdate.AddCard(card)
-		} else {
-			if !card.Equals(oldCards[i]) {
-				err := c.cardRepo.UpdateOne(card)
-				if err != nil {
-					log.Fatalf("Card %d failed to be updated: %v", card.ID, err)
-					continue
-				}
-				log.Printf("Card %d was updated", card.ID)
-				cardsMetaUpdate.UpdateCard(oldCards[i], card)
-			}
-		}
-	}
-
-	err = c.cardMetaRepo.InsertOne(cardsMetaUpdate)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func sortCardsByAlphabet(cards []domain.Card) []domain.Card {
-	sort.Slice(cards, func(i, j int) bool {
-		return cards[i].Name < cards[i].Name
+	// TODO: make sure to save the if the update made changes
+	// currently it only saves that a update happend, but not what happened
+	c.cardMetaRepo.InsertOne(domain.CardMeta{
+		Updated:   time.Now(),
+		IsChanged: false,
 	})
-	return cards
+
+	return c.cardRepo.InsertMany(cards)
 }
